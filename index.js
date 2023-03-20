75aa9d17685961e171055dd6312ebe27ca6187d6
Castro-1 <jecastrog@eafit.edu.co>
2023-03-19 20:23:01 -0500

    database connected, add new element to llist implemented

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
-const handleAdd = () => {
-  const task = document.getElementById("input-field").value;
-  console.log(task);
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+const appSettings = {
+  databaseURL: "https://tasklistapp-f286f-default-rtdb.firebaseio.com/",
 };
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const tasksInDB = ref(database, "tasks");
+
+console.log(app);
+
+const inputFieldEl = document.getElementById("input-field");
+const addButtonEl = document.getElementById("add-button");
+const tasksList = document.getElementById("tasks-list");
+
+addButtonEl.addEventListener("click", function () {
+  let inputValue = inputFieldEl.value;
+  push(tasksInDB, inputValue);
+  tasksList.innerHTML += `<li>${inputValue}</li>`;
+  inputFieldEl.value = "";
+
+  console.log(`${inputValue} added to database`);
+});
